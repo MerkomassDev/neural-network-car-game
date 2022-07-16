4366a1be81ee5351ab191ebe96062ef623a0a7c6
MerkomassUnity <merkomass@gmail.com>
2022-07-17 00:57:10 +0200

    removed a debugging printout of currently pressed keys

--- controls.js
+++ controls.js
@@ -23,7 +23,6 @@ class Controls{
                     this.reverse = true;
                     break;
             }
-            console.table(this)
         }
         document.onkeyup = (event) => {
             switch(event.key){
@@ -40,7 +39,6 @@ class Controls{
                     this.reverse = false;
                     break;
             }
-            console.table(this)
         }
     }
 }
