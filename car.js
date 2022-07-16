3abe0d380cb204a4ecb2976fe01cb02f87d34e51
MerkomassUnity <merkomass@gmail.com>
2022-07-17 00:53:37 +0200

    added turning

--- car.js
+++ car.js
@@ -15,19 +15,19 @@ class Car
         this.maxForwardSpeed=3;
         this.maxReverseSpeed=1.5;
         this.friction=0.05;
+        this.angle=0;
 
         this.controls = new Controls();
     }
 
     update()
     {
-        //forward acceleration
+        //forward acceleration/control
         if(this.controls.forward)
         {
             this.speed+=this.forwardAcceleration;
-
         }
-        //backwards acceleration
+        //backwards acceleration/control
         if(this.controls.reverse)
         {
             this.speed-=this.reverseAcceleration;
@@ -59,19 +59,42 @@ class Car
         {
             this.speed=0;
         }
+
+        //flips left/right in reverse (it was backwards before)
+        if(this.speed!=0)
+        {
+            const flip=this.speed>0?1:-1;
+
+            //left right controls
+            if(this.controls.left)
+            {
+                this.angle+=0.03*flip;
+            }
+            if(this.controls.right)
+            {
+                this.angle-=0.03*flip;
+            }
+        }
         
-        this.y-=this.speed;
+        
+        this.x-=Math.sin(this.angle)*this.speed;
+        this.y-=Math.cos(this.angle)*this.speed;
     }
 
     draw(ctx)
     {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
         ctx.beginPath();
         ctx.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
+            -this.width / 2,
+            -this.height / 2,
             this.width,
             this.height
             );
         ctx.fill();
+
+        ctx.restore();
     }
 }
