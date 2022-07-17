7fbf4f4fe503367d0398fa997e097e297f8dc42d
MerkomassUnity <merkomass@gmail.com>
2022-07-18 00:45:49 +0200

    defined road borders

--- road.js
+++ road.js
@@ -13,6 +13,15 @@ class Road
         this.top=-bigNumber;
         this.bottom=bigNumber;
 
+        const topLeft={x:this.left, y:this.top};
+        const topRight={x:this.right, y:this.top};
+        const bottomLeft={x:this.left, y:this.bottom};
+        const bottomRight={x:this.right, y:this.bottom};
+
+        this.borders=
+        [
+            [topLeft,bottomLeft], [topRight,bottomRight]
+        ]
     }
 
     getLaneCenter(laneIndex)
@@ -26,7 +35,7 @@ class Road
         ctx.lineWidth=5;
         ctx.strokeStyle="white";
 
-        for(let i=0; i<=this.laneCount; i++)
+        for(let i=1; i<=this.laneCount-1; i++)
         {
             const x=lerp
             (
@@ -35,19 +44,22 @@ class Road
                 i/this.laneCount
             )
 
-            if (i>0 && i<this.laneCount)
-            {
-                ctx.setLineDash([20, 20])
-            }
-            else
-            {
-                ctx.setLineDash([])
-            }
+            ctx.setLineDash([20, 20])
 
             ctx.beginPath();
             ctx.moveTo(x, this.top);
             ctx.lineTo(x, this.bottom);
             ctx.stroke();
         }
+
+        ctx.setLineDash([]);
+        this.borders.forEach(border=>
+            {
+                ctx.beginPath();
+                ctx.moveTo(border[0].x, border[0].y);
+                ctx.lineTo(border[1].x, border[1].y);
+                ctx.stroke();
+            }
+            )
     }
 }
